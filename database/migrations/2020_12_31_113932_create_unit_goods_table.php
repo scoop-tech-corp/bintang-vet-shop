cc92d6277d012adb8efa62d1bb8f31bbc3a704b0
ahmedmutar <amx23baradja@gmail.com>
2020-12-31 14:12:33 +0700

    Add Satuan Barang API and Search Kategori Barang API

---
+++ database/migrations/2020_12_31_113932_create_unit_goods_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnitGoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unit_goods', function (Blueprint $table) {
+            $table->id();
+            $table->string('unit_name');
+            $table->boolean('isDeleted')->nullable()->default(false);
+            $table->string('created_by');
+            $table->string('update_by')->nullable();
+            $table->string('deleted_by')->nullable();
+            $table->timestamp('deleted_at',0)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unit_goods');
+    }
+}
