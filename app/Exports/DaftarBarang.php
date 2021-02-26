376bb585d99272effe37d3ff0b12a5e9655cc1b0
ahmedmutar <amx23baradja@gmail.com>
2021-02-26 23:51:39 +0700

    Download Template Daftar Barang

--- app/Exports/DaftarBarang.php
+++ app/Exports/DaftarBarang.php
@@ -2,15 +2,22 @@
 
 namespace App\Exports;
 
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithTitle;
 
-class DaftarBarang implements FromCollection
+class DaftarBarang implements ShouldAutoSize, WithHeadings, WithTitle
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
+
+    public function headings(): array
     {
-        //
+        return [
+            ['Nama Barang', 'Jumlah Barang', 'Kode Satuan Barang', 'Kode Kategori Barang', 'Kode Cabang Barang']
+        ];
+    }
+
+    public function title(): string
+    {
+        return 'Daftar Kategori Barang';
     }
 }
