0307236b0b2ab0880b70b824e44ff0867c10c831
Adiyansyah Dwi Putra <adiyansyahdwiputra@gmail.com>
2021-03-28 22:01:33 +0700

    add selling price in detail hasil pemeriksaan and detail riwayat pemeriksaan

--- public/main/js/pasien/riwayat-pasien.js
+++ public/main/js/pasien/riwayat-pasien.js
@@ -164,7 +164,7 @@ $(document).ready(function() {
         rowKelompokObat += `<div class="target" style="margin-bottom: 30px">`
         + `<div class="m-b-10px" style="font-weight: 700">Kelompok Obat ${no}</div>`
         + `<div class="m-b-10px">`
-        + `${li.group_name} - ${li.branch_name}`
+        + `${li.group_name} - ${li.branch_name} - ${li.selling_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`
         + `</div>`
         + `<div class="table-responsive" id="table-list-barang-${idx}">`
         +   `<table class="table table-striped text-nowrap">`
