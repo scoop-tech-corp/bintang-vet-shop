a927c753ab7284c8f9fa2264228783ac19ba96e7
ahmedmutar <amx23baradja@gmail.com>
2021-04-16 21:36:49 +0700

    Update LaporanKeuanganHarianController.php

--- app/Http/Controllers/LaporanKeuanganHarianController.php
+++ app/Http/Controllers/LaporanKeuanganHarianController.php
@@ -37,9 +37,9 @@ class LaporanKeuanganHarianController extends Controller
                 'patients.id_member as patient_number', 'patients.pet_category', 'patients.pet_name', 'registrations.complaint',
 
                 DB::raw("TRIM(SUM(detail_item_patients.price_overall) + SUM(detail_service_patients.price_overall))+0 as price_overall"),
-                DB::raw("TRIM(SUM(price_items.capital_price) + SUM(price_services.capital_price))+0 as capital_price"),
-                DB::raw("TRIM(SUM(price_items.doctor_fee) + SUM(price_services.doctor_fee))+0 as doctor_fee"),
-                DB::raw("TRIM(SUM(price_items.petshop_fee) + SUM(price_services.petshop_fee))+0 as petshop_fee"),
+                DB::raw("TRIM(SUM(price_items.capital_price * detail_item_patients.quantity) + SUM(price_services.capital_price * detail_service_patients.quantity))+0 as capital_price"),
+                DB::raw("TRIM(SUM(price_items.doctor_fee * detail_item_patients.quantity) + SUM(price_services.doctor_fee * detail_service_patients.quantity))+0 as doctor_fee"),
+                DB::raw("TRIM(SUM(price_items.petshop_fee * detail_item_patients.quantity) + SUM(price_services.petshop_fee * detail_service_patients.quantity))+0 as petshop_fee"),
 
                 'users.fullname as created_by', DB::raw("DATE_FORMAT(list_of_payments.created_at, '%d %b %Y') as created_at"))
             ->groupBy('list_of_payments.id', 'check_up_results.id', 'registrations.id_number', 'patients.id_member', 'patients.pet_category', 'patients.pet_name',
