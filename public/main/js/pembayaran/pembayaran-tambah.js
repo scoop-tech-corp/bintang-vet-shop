c048b959162bf978a87b31866f71ceaea9ccc096
Adiyansyah Dwi Putra <adiyansyahdwiputra@gmail.com>
2021-06-05 23:35:06 +0700

    remove function print

--- public/main/js/pembayaran/pembayaran-tambah.js
+++ public/main/js/pembayaran/pembayaran-tambah.js
@@ -71,79 +71,6 @@ $(document).ready(function() {
     $('#modal-confirmation').modal('show');
   });
 
-  // $('#btnPrint').click(function () {
-  //   const fd = new FormData();
-  //   fd.append('check_up_result_id', 1);
-  //   fd.append('service_payment', JSON.stringify([{detail_service_patient_id: 1}]));
-  //   fd.append('item_payment', JSON.stringify([{detail_item_patient_id:1},{detail_item_patient_id:2},{detail_item_patient_id:3}]));
-  //   $.ajax({
-	// 		url     : $('.baseUrl').val() + '/api/pembayaran/print',
-	// 		headers : { 'Authorization': `Bearer ${token}` },
-	// 		type    : 'POST',
-	// 		data: fd, contentType: false, cache: false,
-  //     processData: false,
-	// 		beforeSend: function() { $('#loading-screen').show(); },
-	// 		success: function(resp) {
-  //       console.log('getDataPrint', resp);
-  //       let table = [];
-  //       let price_overall = resp.price_overall.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-  //       let address = resp.address;
-  //       let cashier_name = resp.cashier_name;
-  //       let id_patient = resp.id_patient;
-  //       let owner_name = resp.owner_name;
-  //       let pet_name = resp.pet_name;
-  //       let quantity_total = resp.quantity_total;
-  //       let registration_number = resp.registration_number;
-  //       let time = resp.time;
-
-  //       resp.data_service.forEach(ds => {
-  //         table.push({name: ds.item_name, qty: ds.quantity, 
-  //           harga: ds.selling_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.'), 
-  //           total: ds.price_overall.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')});
-  //       });
-  //       resp.data_item.forEach(di => {
-  //         table.push({name: di.item_name, qty: di.quantity, 
-  //           harga: di.selling_price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.'), 
-  //           total: di.price_overall.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')});
-  //       });        
-
-  //       const datas = {
-  //         _token: $('meta[name="csrf-token"]').attr('content'),
-  //         table, price_overall, address, cashier_name, id_patient,
-  //         owner_name, pet_name, quantity_total, registration_number,
-  //         time
-  //       };
-
-  //       $.ajax({
-  //         url : $('.baseUrl').val() + '/print',
-  //         type: 'POST',
-  //         dataType: 'JSON',
-  //         headers: { 'Authorization': `Bearer ${token}` },
-  //         data: datas,
-  //         beforeSend: function() { $('#loading-screen').show(); },
-  //         success: function(resp) {
-  //           console.log('print berhasil');
-          
-  //         }, complete: function() { $('#loading-screen').hide(); }
-  //         , error: function(err) {
-  //           console.log('print gagal');
-  //           if (err.status == 401) {
-  //             localStorage.removeItem('vet-clinic');
-  //             location.href = $('.baseUrl').val() + '/masuk';
-  //           }
-  //         }
-  //       });
-	// 		}, complete: function() { $('#loading-screen').hide(); },
-	// 		error: function(err) {
-	// 			if (err.status == 401) {
-	// 				localStorage.removeItem('vet-clinic');
-	// 				location.href = $('.baseUrl').val() + '/masuk';
-	// 			}
-	// 		}
-	// 	});
-
-  // });
-
   function processSaved() {
 
     let finalSelectedJasa = []; let finalSelectedBarang = [];
@@ -169,18 +96,6 @@ $(document).ready(function() {
       processData: false,
       beforeSend: function() { $('#loading-screen').show(); },
       success: function(resp) {
-
-        let disposition = xhr.getResponseHeader('content-disposition');
-				let matches = /"([^"]*)"/.exec(disposition);
-				let filename = (matches != null && matches[1] ? matches[1] : 'file.pdf');
-				let blob = new Blob([data],{type:'application/pdf'});
-				let downloadUrl = URL.createObjectURL(blob);
-				let a = document.createElement("a");
-
-				a.href = downloadUrl;
-				a.download = filename
-				document.body.appendChild(a);
-				a.click();
 
         $("#msg-box .modal-body").text('Berhasil Menambah Data');
         $('#msg-box').modal('show');
