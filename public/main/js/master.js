9fdb17a05a3e8528a1d66d956f79d1718e9ef7af
Adiyansyah Dwi Putra <adiyansyahdwiputra@gmail.com>
2021-06-05 15:03:32 +0700

    fix typo jumat in header time

--- public/main/js/master.js
+++ public/main/js/master.js
@@ -124,7 +124,7 @@ $(document).ready(function() {
 
   function showTime() {
     let months  = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
-    let myDays  = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum&#39;at', 'Sabtu'];
+    let myDays  = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
     let date    = new Date();
     let day     = date.getDate();
     let month   = date.getMonth();
