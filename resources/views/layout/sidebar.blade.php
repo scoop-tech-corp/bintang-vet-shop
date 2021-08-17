ef379044c00eeb9015415ea02e623bc33763cb01
Adiyansyah Dwi Putra <adiyansyahdwiputra@gmail.com>
2021-08-17 17:32:58 +0700

    change color bintang vet shop

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,6 +1,4 @@
-<!-- Left side column. contains the logo and sidebar -->
 <aside class="main-sidebar">
-	<!-- sidebar: style can be found in sidebar.less -->
 	<section class="sidebar">
 		<div class="section-logo-vet-clinic">
 			<img src="{{ asset('assets/image/logo-vet-clinic.jpg') }}">
@@ -9,51 +7,6 @@
 			<li class="header">MENU UTAMA</li>
 			<li class="menuCabang"><a href="{{ url('/cabang') }}"><i class="fa fa-code-fork"></i> <span>Cabang</span></a></li>
 			<li class="menuUser"><a href="{{ url('/user') }}"><i class="fa fa-users" aria-hidden="true"></i> <span>User Management</span></a></li>
-			<li class="menuPasien"><a href="{{ url('/pasien') }}"><i class="fa fa-bed"></i> <span>Pendaftaran Pasien</span></a></li>
-			<li class="menuPendaftaran"><a href="{{ url('/pendaftaran') }}"><i class="fa fa-file-text-o"></i> <span>Pendaftaran Berobat</span></a></li>
-			{{-- <li class="treeview menuPendaftaran">
-				<a href="#">
-					<i class="fa fa-file-text-o" aria-hidden="true"></i>
-					<span>Pendaftaran</span>
-					<span class="pull-right-container">
-						<i class="fa fa-angle-left pull-right"></i>
-					</span>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="{{ url('/rawat-jalan') }}"><i class="fa fa-circle-o"></i> Rawat Jalan</a></li>
-					<li><a href="{{ url('/rawat-inap') }}"><i class="fa fa-circle-o"></i> Rawat Inap</a></li>
-				</ul>
-			</li> --}}
-			<li class="menuDokter"><a href="{{ url('/penerimaan-pasien') }}"><i class="fa fa-id-card"></i> <span>Penerimaan Pasien</span></a></li>
-			{{-- <li class="treeview menuDokter">
-				<a href="#">
-					<i class="fa fa-stethoscope" aria-hidden="true"></i>
-					<span>Dokter</span>
-					<span class="pull-right-container">
-						<i class="fa fa-angle-left pull-right"></i>
-					</span>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="{{ url('/dokter-rawat-jalan') }}"><i class="fa fa-circle-o"></i> Rawat Jalan</a></li>
-					<li><a href="{{ url('/dokter-rawat-inap') }}"><i class="fa fa-circle-o"></i> Rawat Inap</a></li>
-				</ul>
-			</li> --}}
-			<li class="menuPeriksa"><a href="{{ url('/hasil-pemeriksaan') }}"><i class="fa fa-heartbeat" aria-hidden="true"></i> <span>Hasil Pemeriksaan</span></a></li>
-			{{-- <li class="menuTindakan"><a href="{{ url('/tindakan') }}"><i class="fa fa-medkit" aria-hidden="true"></i> <span>Tindakan</span></a></li> --}}
-			<li class="treeview menuLayanan">
-				<a>
-					<i class="fa fa-user-md" aria-hidden="true"></i>
-					<span>Layanan</span>
-					<span class="pull-right-container">
-						<i class="fa fa-angle-left pull-right"></i>
-					</span>
-				</a>
-				<ul class="treeview-menu">
-					<li><a href="{{ url('/kategori-jasa') }}"><i class="fa fa-circle-o"></i> Kategori Jasa</a></li>
-					<li><a href="{{ url('/daftar-jasa') }}"><i class="fa fa-circle-o"></i> Daftar Jasa</a></li>
-					<li><a href="{{ url('/pembagian-harga-jasa') }}"><i class="fa fa-circle-o"></i> Pembagian Harga Jasa</a></li>
-				</ul>
-			</li>
 			<li class="treeview menuGudang">
 				<a>
 					<i class="fa fa-database" aria-hidden="true"></i>
@@ -63,12 +16,14 @@
 					</span>
 				</a>
 				<ul class="treeview-menu">
-					<li><a href="{{ url('/kategori-barang') }}"><i class="fa fa-circle-o"></i> Kategori Barang</a></li>
-					<li><a href="{{ url('/satuan-barang') }}"><i class="fa fa-circle-o"></i> Satuan Barang</a></li>
-					<li><a href="{{ url('/daftar-barang') }}"><i class="fa fa-circle-o"></i> Daftar Barang</a></li>
-					<li><a href="{{ url('/pembagian-harga-barang') }}"><i class="fa fa-circle-o"></i> Pembagian Harga Barang</a></li>
-					<li><a href="{{ url('/kelompok-obat') }}"><i class="fa fa-circle-o"></i> Kelompok Obat</a></li>
-					<li><a href="{{ url('/pembagian-harga-kelompok-obat') }}"><i class="fa fa-circle-o"></i> Harga Kelompok Obat</a></li>
+					<li><a href="{{ url('/kategori-barang') }}"><i class="fa fa-circle-o"></i> Cat Food</a></li>
+					<li><a href="{{ url('/satuan-barang') }}"><i class="fa fa-circle-o"></i> Dog Food</a></li>
+					<li><a href="{{ url('/daftar-barang') }}"><i class="fa fa-circle-o"></i> Animal Food</a></li>
+					<li><a href="{{ url('/pembagian-harga-barang') }}"><i class="fa fa-circle-o"></i> Vitamin</a></li>
+					<li><a href="{{ url('/kelompok-obat') }}"><i class="fa fa-circle-o"></i> Pet Care</a></li>
+					<li><a href="{{ url('/pembagian-harga-kelompok-obat') }}"><i class="fa fa-circle-o"></i> Kandang</a></li>
+          <li><a href="{{ url('/pembagian-harga-kelompok-obat') }}"><i class="fa fa-circle-o"></i> Aksesoris</a></li>
+          <li><a href="{{ url('/pembagian-harga-kelompok-obat') }}"><i class="fa fa-circle-o"></i> Lain-lain</a></li>
 				</ul>
 			</li>
 			<li class="menuPembayaran"><a href="{{ url('/pembayaran') }}"><i class="fa fa-money" aria-hidden="true"></i> <span>Pembayaran</span></a></li>
@@ -84,11 +39,8 @@
 					<li><a href="{{ url('/laporan-keuangan-harian') }}"><i class="fa fa-circle-o"></i> Harian</a></li>
 					<li><a href="{{ url('/laporan-keuangan-mingguan') }}"><i class="fa fa-circle-o"></i> Mingguan</a></li>
 					<li><a href="{{ url('/laporan-keuangan-bulanan') }}"><i class="fa fa-circle-o"></i> Bulanan</a></li>
-					{{-- <li><a><i class="fa fa-circle-o"></i> Tahunan</a></li> --}}
 				</ul>
 			</li>
-			{{-- <li class="menuKunjungan"><a href="{{ url('/kunjungan') }}"><i class="fa fa-wheelchair" aria-hidden="true"></i> <span>Kunjungan</span></a></li> --}}
 		</ul>
 	</section>
-	<!-- /.sidebar -->
 </aside>
