09760e4b38b2b9cde5636742595d9dee50f0586f
Project-web-code-program <71145562+Project-web-code-program@users.noreply.github.com>
2022-07-23 11:07:33 +0700

    Update UploadDataItem.php

--- app/Imports/UploadDataItem.php
+++ app/Imports/UploadDataItem.php
@@ -7,6 +7,7 @@ use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithValidation;
+use Carbon\Carbon;
 
 class UploadDataItem implements ToModel, WithHeadingRow, WithValidation
 {
@@ -22,7 +23,8 @@ class UploadDataItem implements ToModel, WithHeadingRow, WithValidation
 
     public function model(array $row)
     {
-        $exp_date = \Carbon\Carbon::parse(Carbon::createFromFormat('d/m/Y', $row['tanggal_kedaluwarsa_barang_ddmmyyyy'])->format('Y/m/d'));
+        $exp_date = $exp_date = Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['tanggal_kedaluwarsa_barang_ddmmyyyy']));
+        //Carbon::parse(Carbon::createFromFormat('d/m/Y', $row['tanggal_kedaluwarsa_barang_ddmmyyyy'])->format('Y/m/d'));
         return new ListofItems([
             'item_name' => $row['nama_barang'],
             'total_item' => $row['jumlah_barang'],
@@ -35,7 +37,7 @@ class UploadDataItem implements ToModel, WithHeadingRow, WithValidation
             'category' => $this->category,
             'branch_id' => $row['kode_cabang'],
             'diff_item' => $row['jumlah_barang'] - $row['limit_barang'],
-            'diff_expired_days' => \Carbon\Carbon::parse(now())->diffInDays($exp_date, false),
+            'diff_expired_days' => Carbon::parse(now())->diffInDays($exp_date, false),
             'user_id' => $this->id,
         ]);
     }
@@ -46,7 +48,7 @@ class UploadDataItem implements ToModel, WithHeadingRow, WithValidation
             '*.nama_barang' => 'required|string',
             '*.jumlah_barang' => 'required|numeric',
             '*.limit_barang' => 'required|numeric',
-            '*.tanggal_kedaluwarsa_barang_ddmmyyyy' => 'required|date_format:d/m/Y',
+            '*.tanggal_kedaluwarsa_barang_ddmmyyyy' => 'required',
             '*.harga_jual' => 'required|numeric',
             '*.harga_modal' => 'required|numeric',
             '*.kode_cabang' => 'required|numeric',
